67c9fed28ffa27e86ccba6eaae91ee2f94979386
Juan Jose Vega <juanjosevegaquintero@hotmail.com>
2020-05-29 18:54:07 -0500

    script part 1 finish

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,9 @@
-const functions = require('firebase-functions');
+const functions = require("firebase-functions");
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+exports.api = functions.https.onRequest((request, response) => {
+  if (request.method === "GET") {
+    response.json({ hi: "hi" });
+  } else {
+    response.status(500).send("I failed you");
+  }
+});
