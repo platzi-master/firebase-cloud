9680ad7cba6917b91be84d7ae9f755404a9dcb3b
Juan Jose Vega <juanjosevegaquintero@hotmail.com>
2020-05-29 20:49:00 -0500

    update live coding

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,44 @@
 const functions = require("firebase-functions");
+const admin = require("firebase-admin");
+const serviceAccount = require("./serviceAccountKey.json");
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://juanjo-fc.firebaseio.com",
+});
+
+const data = {
+  name: "Juan Jose",
+  lastName: "Vega",
+  description:
+    "I'm Software Developer specialist in Backend and Web Technologies, I live and breathe software. Currently focusing on JS and Python in Platzi Master",
+};
+
+const db = admin.database();
+const ref = db.ref("/assistant");
+const childRef = ref.child("technical");
 
 exports.api = functions.https.onRequest((request, response) => {
   if (request.method === "GET") {
-    response.json({ hi: "hi" });
+    const data = childRef;
+    data.on("value", (snapshot) => {
+      const parseData = Object.keys(snapshot.val()).map(
+        (key) => snapshot.val()[key]
+      );
+      response.json(parserData);
+    });
+    // response.json(data);
   } else {
     response.status(500).send("I failed you");
+  }
+});
+
+exports.saveData = functions.https.onRequest((request, response) => {
+  if (request.method === "POST") {
+    const data = childRef.push();
+    data.set({});
+    response.status(200).json(request.body);
+  } else {
+    response.status(500).send("You don't have access");
   }
 });
